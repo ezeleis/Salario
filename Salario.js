d203bfb85b7331204db821b17d06e3ef8ffe45da
Facundo <facundo@MacBook-Pro-de-Mac-2.local>
2023-02-13 20:28:21 -0300

    updated Salario as a function

--- Salario.js
+++ Salario.js
@@ -1,7 +1,12 @@
 var salarioInic = prompt("Insira o salario inicial");
 var metaSalarial = prompt("Insira uma meta a atingir");
+var salario = salario
 
-for (i=0; salarioInic<metaSalarial;i++){
-  salarioInic=salarioInic*1.1;
-} 
-alert(`Precisa de `+i*6+` meses de trabalho (ou ${i} semestre/s) para atingir o seu salario desejado de R$${metaSalarial}`);
+function calculoSalario(salarioInic,metaSalarial){
+  for (var i=0;salarioInic<metaSalarial;i++){
+    salarioInic=salarioInic*1.1; 
+  }
+  return i*6;
+}
+
+alert(`Precisa de ${calculoSalario(salarioInic,metaSalarial)} meses de trabalho  para atingir o seu salario desejado de R$${metaSalarial}`);
