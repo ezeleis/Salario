ecd682789e25031f01c4a32ffd206a70c202cf0c
Facundo <facundo@MacBook-Pro-de-Mac-2.local>
2023-02-13 19:33:53 -0300

    simplified salario.js

--- Salario.js
+++ Salario.js
@@ -1,8 +1,7 @@
 var salarioInic = prompt("Insira o salario inicial");
 var metaSalarial = prompt("Insira uma meta a atingir");
-var salario = salarioInic;
 
-for (i=0; salario<metaSalarial;i++){
-  salario=salario*1.1;
+for (i=0; salarioInic<metaSalarial;i++){
+  salarioInic=salarioInic*1.1;
 } 
 alert(`Precisa de `+i*6+` meses de trabalho (ou ${i} semestre/s) para atingir o seu salario desejado de R$${metaSalarial}`);
